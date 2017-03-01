637eccc566651c455a54f1496326306cb7f90141
nathanfaucett <nathanfaucett@gmail.com>
2017-03-01 11:19:58 -0600

    clean up

--- src/collection.rs
+++ src/collection.rs
@@ -2,6 +2,7 @@
 
 pub trait Collection {
     fn len(&self) -> usize;
+    fn clear(&mut self);
     #[inline(always)]
     fn is_empty(&self) -> bool {
         self.len() == 0
--- src/deque.rs
+++ src/deque.rs
@@ -2,11 +2,11 @@ use super::collection::Collection;
 
 
 pub trait Deque<T>: Collection {
-
-    fn push_back(&mut self, element: T);
     fn push_front(&mut self, element: T);
-    fn pop_back(&mut self) -> Option<T>;
+    fn push_back(&mut self, element: T);
+
     fn pop_front(&mut self) -> Option<T>;
+    fn pop_back(&mut self) -> Option<T>;
 
     fn front(&self) -> Option<&T>;
     fn back(&self) -> Option<&T>;
