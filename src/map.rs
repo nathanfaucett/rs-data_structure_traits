8cf310a7824ce57f8b844cfb6c58a40378f98e47
nathanfaucett <nathanfaucett@gmail.com>
2017-03-07 13:11:30 -0600

    clean up

--- src/map.rs
+++ src/map.rs
@@ -4,10 +4,10 @@ use super::collection::Collection;
 use super::iterable::Iterable;
 
 
-pub trait Map<'a, 'b, Key: 'a, Value: 'b>:
+pub trait Map<'a, Key: 'a, Value: 'a>:
     Collection +
     Index<&'a Key, Output = Value> +
-    Iterable<'a, (&'a Key, &'b Value)>
+    Iterable<'a, (&'a Key, &'a Value)>
 {
     fn contains_key(&self, key: &'a Key) -> bool;
 }
--- src/map_mut.rs
+++ src/map_mut.rs
@@ -6,9 +6,9 @@ use super::map::Map;
 use super::remove::Remove;
 
 
-pub trait MapMut<'a, 'b, Key: 'a, Value: 'b>:
-    Map<'a, 'b, Key, Value> +
+pub trait MapMut<'a, Key: 'a, Value: 'a>:
+    Map<'a, Key, Value> +
     Insert<Key, Value, Output = Option<Value>> +
     Remove<&'a Key, Output = Value> +
     IndexMut<&'a Key, Output = Value> +
-    IterableMut<'a, (&'a mut Key, &'b mut Value)> {}
+    IterableMut<'a, (&'a Key, &'a mut Value)> {}
